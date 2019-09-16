15f8921cff6ca02c221bae0288bbab840793a05a
phatdroid94 <phatdroid94@gmail.com>
2019-09-16 21:07:57 +0700

    tham chieu va tham tri

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/ungdungdautien04092019/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/ungdungdautien04092019/MainActivity.java
@@ -4,46 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView mtxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //1 : Array : xem , sua
-        // Khoi tao
-//        String[] mangTens = new String[5];
-        //Chinh sua
-//        mangTens[0] ="Hoa";
-//        mangTens[1] ="Ti";
-//        mangTens[2] ="Hue";
-//        mangTens[3] ="Lan";
-//        mangTens[4] ="Tuan";
-//        String mangten[] = {"Hoa","Hue","Lan","Ngoc","Tuan"};
-//        for (int i = 0 ; i < mangTens.length ; i++){
-//            //ctrl +  P : xem tham so truyen vao
-//            Log.d("BBB", mangTens[i]);
-//        }
-        // 2 : Arraylist
-        //khoi tao:
-
-        //type
-        //ctrl + space : phim goi y code
-        ArrayList<Integer> mangintegers = new ArrayList<>();
-        //Them gia tri
-        mangintegers.add(1);
-        mangintegers.add(2);
-        mangintegers.add(3);
-        // Xem kich thuoc mang
-        Log.d("BBB", String.valueOf(mangintegers.size()));
-        // Xoa
-        mangintegers.remove(0);
-        Log.d("BBB", String.valueOf(mangintegers.size()));
-        mangintegers.set(0,5);
-        Log.d("BBB", mangintegers.get(0).toString());
+//        // Nguyen thuy
+//        int a = 5;
+//        int b = 4;
+//
+//        a = b
+//        a thay doi , b khong thay doi : vi b truyen tham tri
+//        // Kieu object
+//        Integer a = 5;
+//        Integer b = 4;
+//
+//        a = b
+//        a thay doi , b thay doi : vi b truyen tham chieu
     }
 }
